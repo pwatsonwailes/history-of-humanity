981af816d6d6d6389022db652c5d5e465285b3fc
Builtvisible <pete@builtvisible.com>
2015-10-29 13:19:21 +0000

    script init amends

--- app/main.js
+++ app/main.js
@@ -1,28 +1,50 @@
 var React = require("react/addons"),
-  ReactCSSTransitionGroup = React.addons.CSSTransitionGroup,
-  axios = require('axios'),
-  timelineJsonData = require('./data/timeline.json'),
-  HoH = require('./js/dispatcher.js');
+	ReactCSSTransitionGroup = React.addons.CSSTransitionGroup,
+	axios = require('axios'),
+	timelineJsonData = require('./data/timeline.json'),
+	HoH = require('./js/dispatcher.js');
 
 window.app = (function() {
-  var requiredFeatures = {
-    "JSON decoding": window.JSON,
-    "the selectors API": document.querySelector,
-    "DOM level 2 events": window.addEventListener,
-    "the HTML5 history API": window.history.pushState
-  };
+	var requiredFeatures = {
+		"JSON decoding": window.JSON,
+		"the selectors API": document.querySelector,
+		"DOM level 2 events": window.addEventListener,
+		"the HTML5 history API": window.history.pushState
+	};
 
-  for (var i = requiredFeatures.length - 1; i >= 0; i--) {
-    if (!requiredFeatures[i])
-      return alert("Sorry, but your browser does not support " + feature + " so this app won't work properly.");
-  };
+	for (var i = requiredFeatures.length - 1; i >= 0; i--) {
+		if (!requiredFeatures[i])
+			return alert("Sorry, but your browser does not support " + feature + " so this app won't work properly.");
+	};
 
-  if (window.location.pathname !== '/history-of-humanity/') {
-    var parts = window.location.pathname.replace('/history-of-humanity/', '').split('/');
-    var initData = { year: parts[0], position: parts[1], name: parts[2] }
-  }
-  else
-    var initData = { year: false, position: false, name: false }
+	if (window.location.pathname !== '/history-of-humanity/') {
+		var parts = window.location.pathname.replace('/history-of-humanity/', '').split('/');
+		var params = { year: parts[0], position: parts[1], name: parts[2] };
 
-  return React.render(React.createElement(HoH, { timeline: timelineJsonData, initparams: initData }), document.getElementById('hoh'));
+    var initData = {
+      itemDetail: timelineJsonData[req.params.year][req.params.position],
+      wikiData: false,
+      wikiImages: []
+    };
+
+		var wikiApiLink = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|images&exintro=&explaintext=&titles=' + params.name;
+
+		axios.get('https://apis.builtvisible.com/history_of_humanity/?url=' + encodeURIComponent(wikiApiLink.replace(/&amp;/g, "&"))).then(function (output) {
+			if (isset(output.data.query.pages)) {
+				var pageId = Object.keys(output.data.query.pages);
+				initData.wikiData = output.data.query.pages[pageId];
+
+				// React.renderToString takes your component and generates rendered markup. SEO friendliness all the way
+				return React.render(React.createElement(HoH, { timeline: timelineJsonData, initparams: params, initwikidata: initData }), document.getElementById('hoh'));
+			}
+		})
+		.catch(function (e) {
+			console.log('error in xhr');
+			console.log(e);
+		});
+	}
+	else {
+		return React.render(React.createElement(HoH, { timeline: timelineJsonData, initparams: { year: false, position: false, name: false } }), document.getElementById('hoh'));
+	}
+
 })();
--- app/routes/core-routes.js
+++ app/routes/core-routes.js
@@ -8,35 +8,32 @@ function isset (obj) { return typeof obj !== 'undefined'; }
 module.exports = function(app) {
 	app.get('/', function(req, res) {
 		// React.renderToString takes your component and generates rendered markup. SEO friendliness all the way
-		var staticHTML = React.renderToString(HoH ({ timeline: timelineJsonData, initParams: false }));
+		var staticHTML = React.renderToString(HoH ({ timeline: timelineJsonData, initparams: false }));
 		res.render('index.ejs', { reactOutput: staticHTML });
 	});
 
 	app.get('/:year/:position/:name', function(req, res) {
-		function getWikiData (wikiApiLink, renderer) {
-			var initData = {
-				itemDetail: timelineJsonData[req.params.year][req.params.position],
-				wikiData: false,
-				wikiImages: []
-			};
-
-			axios.get('https://apis.builtvisible.com/history_of_humanity/?url=' + encodeURIComponent(wikiApiLink.replace(/&amp;/g, "&"))).then(function (output) {
-				if (isset(output.data.query.pages)) {
-					var pageId = Object.keys(output.data.query.pages);
-					initData.wikiData = output.data.query.pages[pageId];
-
-					// React.renderToString takes your component and generates rendered markup. SEO friendliness all the way
-					var staticHTML = React.renderToString(HoH ({ timeline: timelineJsonData, initparams: req.params, initwikidata: initData }));
-					renderer.render('index.ejs', { reactOutput: staticHTML });
-				}
-			})
-			.catch(function (e) {
-				console.log('error in xhr');
-				console.log(e);
-			});
-		}
+		var initData = {
+			itemDetail: timelineJsonData[req.params.year][req.params.position],
+			wikiData: false,
+			wikiImages: []
+		};
 
 		var wikiApiLink = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts|images&exintro=&explaintext=&titles=' + req.params.name;
-		getWikiData(wikiApiLink, res);
+
+		axios.get('https://apis.builtvisible.com/history_of_humanity/?url=' + encodeURIComponent(wikiApiLink.replace(/&amp;/g, "&"))).then(function (output) {
+			if (isset(output.data.query.pages)) {
+				var pageId = Object.keys(output.data.query.pages);
+				initData.wikiData = output.data.query.pages[pageId];
+
+				// React.renderToString takes your component and generates rendered markup. SEO friendliness all the way
+				var staticHTML = React.renderToString(HoH ({ timeline: timelineJsonData, initparams: req.params, initwikidata: initData }));
+				res.render('index.ejs', { reactOutput: staticHTML });
+			}
+		})
+		.catch(function (e) {
+			console.log('error in xhr');
+			console.log(e);
+		});
 	});
 };
