6ccb18f27fd1615bd18e8b0d18b413d981532a46
Builtvisible <pete@builtvisible.com>
2015-11-05 16:13:38 +0000

    removed classie

--- public/functions.js
+++ public/functions.js
@@ -22,17 +22,6 @@ function isset (obj) { return typeof obj !== 'undefined'; }
 
 /*
 ---
-name: classie 1.0.1
-description: makes number formatting not awful
-//github.com/ded/bonzo
-MIT license
-...
-*/
-
-!function(s){"use strict";function e(s){return new RegExp("(^|\\s+)"+s+"(\\s+|$)")}function n(s,e){var n=t(s,e)?c:a;n(s,e)}var t,a,c;"classList"in document.documentElement?(t=function(s,e){return s.classList.contains(e)},a=function(s,e){s.classList.add(e)},c=function(s,e){s.classList.remove(e)}):(t=function(s,n){return e(n).test(s.className)},a=function(s,e){t(s,e)||(s.className=s.className+" "+e)},c=function(s,n){s.className=s.className.replace(e(n)," ")});var o={hasClass:t,addClass:a,removeClass:c,toggleClass:n,has:t,add:a,remove:c,toggle:n};"function"==typeof define&&define.amd?define(o):"object"==typeof exports?module.exports=o:s.classie=o}(window);
-
-/*
----
 name: mobileCheck
 description: Checks if we're on a mobile device
 details: inspired by //stackoverflow.com/a/11381730/989439
